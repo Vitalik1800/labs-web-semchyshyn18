ca10327b9f73aa081114529879cdbfeff41543a5
Vitalik1800 <51854958+Vitalik1800@users.noreply.github.com>
2022-09-09 09:29:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<style type="text/css">
+		a{
+			text-decoration: none;
+		}
+	</style>
+</head>
+<body>
+	<a href="lab11/lab11.php">Лабораторна робота №11</a>
+</body>
+</html>
