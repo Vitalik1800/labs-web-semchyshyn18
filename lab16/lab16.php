d4cbf8b5a3fdfbbd103c0a8c57bf144f351a7de1
Vitalik1800 <51854958+Vitalik1800@users.noreply.github.com>
2022-10-29 23:57:19 +0300

    Create lab16.php

---
+++ lab16/lab16.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<style type="text/css">
+		a{
+			text-decoration: none;
+		}
+	</style>
+</head>
+<body>
+	<a href="lab16_1.php">Завдання 1</a><br>
+	<a href="lab16_2.php">Завдання 2</a><br>
+</body>
+</html>
