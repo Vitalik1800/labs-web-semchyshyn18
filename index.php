764854c471af10dfb4401a27292162c9884f2681
Vitalik1800 <51854958+Vitalik1800@users.noreply.github.com>
2022-11-11 11:26:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Лабораторні роботи з курсу веб-технологій</title>
+	<title>Document</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<style type="text/css">
 		a{
@@ -19,5 +19,6 @@
 	<a href="lab15/lab15.php">Лабораторна робота №15</a><br>
 	<a href="lab16/lab16.php">Лабораторна робота №16</a><br>
 	<a href="lab17/lab17.php">Лабораторна робота №17</a><br>
+	<a href="lab18/lab18.php">Лабораторна робота №18</a><br>
 </body>
 </html>
